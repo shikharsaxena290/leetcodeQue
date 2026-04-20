f95e33bb84205f2f7d1489d8a23fdf0f7ba99186
shikhar saxena <153847084+shikharsaxena290@users.noreply.github.com>
2026-04-21 00:15:38 +0530

    Time: 128 ms (5.32%) | Memory: 47.5 MB (10.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String out = "";
-        Boolean isplain=false;
         for (int i = 0; i < s.length(); i++) {
             if (Character.isLetterOrDigit(s.charAt(i))) {
                 out += Character.toLowerCase(s.charAt(i));
@@ -10,10 +9,8 @@ class Solution {
 
         for(int i=0;i<out.length()/2;i++){  
             if(out.charAt(i)!=out.charAt((out.length()-1)-i)){
-                isplain= false;
                 return false;
             }
-            isplain= true;
         }
         return true;
     }
